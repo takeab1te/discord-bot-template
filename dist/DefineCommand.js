57b2917fdbb615834f4a623a9ddb8070257a9ff8
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2024-01-20 11:39:54 -0500

    oops

--- dist/DefineCommand.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DefineCommand = void 0;
-function DefineCommand(options) {
-    return options;
-}
-exports.DefineCommand = DefineCommand;
--- dist/Plugin.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DefinePlugin = void 0;
-function DefinePlugin(options) {
-    return options;
-}
-exports.DefinePlugin = DefinePlugin;
