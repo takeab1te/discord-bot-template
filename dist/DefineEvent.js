066a3f7dd18ffdd25b8ba83d31aca591de76492a
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2024-01-20 11:35:15 -0500

    Delete dist directory

--- dist/DefineEvent.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DefineEvent = void 0;
-function DefineEvent(options) {
-    return options;
-}
-exports.DefineEvent = DefineEvent;
